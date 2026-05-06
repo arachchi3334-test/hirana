878d9372ba93cb3d800f789499e070b892b5d7cf
arachchi3334-Test <arachchi3334@gmail.com>
2026-05-06 11:25:10 +0530

    Delete hello.js

--- hello.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => res.send('iapp INNODATA!'));
-app.listen(port, () => console.log(`Server running at http://3.95.160.192:${port}`));
